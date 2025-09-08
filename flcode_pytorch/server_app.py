56a63e61dc83342502dbdf6bdfdc4e24a70089b0
oabuhamdan <oabuhamdan@pm.me>
2025-09-08 13:42:28 -0500

    - Use static paths for in-container data, logs, and configs paths.

--- flcode_pytorch/server_app.py
+++ flcode_pytorch/server_app.py
@@ -4,31 +4,36 @@ from flwr.common import Context
 from flwr.server import ServerApp, ServerAppComponents
 
 from common.configs import *
+from common.dataset_utils import DatasetConfig
 from common.loggers import *
 from flcode_pytorch.utils.contexts import ServerContext
 from .my_client_manager import MyClientManager
 from .my_server import MyServer
 from .utils.model_utils import Net
+from common.static import CONTAINER_LOG_PATH, CONTAINER_CONFIG_PATH
 from .utils.strategy_utils import get_strategy
 
 
 def server_fn(context: Context):
-    general_cfg: GeneralConfig = setup_config("general")
-    server_cfg: ServerConfig = setup_config("server")
-
-    log_file = Path(general_cfg.log_path) / "server.log"
-    init_zmq("default", general_cfg.zmq_ip_address, general_cfg.zmq_port) if server_cfg.zmq_publish else None
-    configure_logger("default", server_cfg.log_to_stream, log_file, general_cfg.logging_level)
+    server_cfg: ServerConfig = setup_config(CONTAINER_CONFIG_PATH, "server", ServerConfig)
+    dataset_cfg: DatasetConfig = setup_config(CONTAINER_CONFIG_PATH, "dataset", DatasetConfig)
+    log_file = Path(CONTAINER_LOG_PATH) / "server.log"
+    if server_cfg.zmq["enable"]:
+        zmq_ip_address = server_cfg.zmq["host"]
+        zmq_port = server_cfg.zmq["port"]
+        init_zmq("default", zmq_ip_address, zmq_port)
+    configure_logger("default", server_cfg.log_to_stream, log_file, server_cfg.logging_level)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     ctx = ServerContext(
         flwr_ctx=context,
-        general_cfg=general_cfg,
         server_cfg=server_cfg,
+        dataset_cfg=dataset_cfg,
         device=device
     )
 
     strategy = get_strategy(ctx, model=Net())
+    print(f"Using strategy: {strategy}")
     client_manager = MyClientManager(ctx)
     config = flwr.server.ServerConfig(num_rounds=server_cfg.num_rounds)
     server = MyServer(ctx, client_manager=client_manager, strategy=strategy)
