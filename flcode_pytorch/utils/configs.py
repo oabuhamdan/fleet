afd632452054f731deb7e38bb7fdd04d52f00d35
oabuhamdan <oabuhamdan@pm.me>
2025-09-08 10:15:18 -0500

    - Defining ZMQ configs as a dict in client and server. - Moving some params from general to be client and server sepcific

--- flcode_pytorch/utils/configs.py
+++ flcode_pytorch/utils/configs.py
@@ -5,11 +5,12 @@ from typing import Any, Dict
 @dataclass
 class ServerConfig:
     log_to_stream: bool = True
+    logging_level : str = "INFO"
     strategy: str = "FedAvg"
     min_fit_clients: int = 1
     min_evaluate_clients: int = min_fit_clients
     min_available_clients: int = min_fit_clients
-    num_rounds: int = 10
+    num_rounds: int = 1
     fraction_fit: float = 1
     fraction_evaluate: float = 1
     server_evaluation: bool = False
@@ -19,20 +20,21 @@ class ServerConfig:
     accuracy_level: float = 0.8
     collect_metrics: bool = False
     collect_metrics_interval: int = 60
-    zmq_publish: bool = False
+    zmq: Dict[str, Any] = field(default_factory=lambda: {"enable":False, "host": "localhost", "port": 5555})
     extra: Dict[str, Any] = field(default_factory=dict)
 
 
 @dataclass
 class ClientConfig:
     log_to_stream: bool = True
+    logging_level : str = "INFO"
     train_batch_size: int = 32
     val_batch_size: int = 128
     local_epochs: int = 1
     learning_rate: float = 1e-3
     log_interval: int = 100
-    collect_metrics: bool = True
+    collect_metrics: bool = False
     collect_metrics_interval: int = 5
     server_address: str = "tcp://localhost:5555"
-    zmq_publish: bool = False
+    zmq: Dict[str, Any] = field(default_factory=lambda: {"enable": False, "host": "localhost", "port": 5555})
     extra: Dict[str, Any] = field(default_factory=dict)
