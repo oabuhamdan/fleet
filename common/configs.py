16295ebc228b8b9a87b20a23f822d10c88ad3c24
oabuhamdan <oabuhamdan@pm.me>
2025-09-08 10:13:31 -0500

    - Adding new fields to general configs - Enhancing the way configs are read inside the containers with OmegaConf

--- common/configs.py
+++ common/configs.py
@@ -1,5 +1,4 @@
 # config_manager.py
-import os
 from dataclasses import dataclass, field
 from typing import Any, Dict
 
@@ -13,8 +12,9 @@ from flcode_pytorch.utils.configs import ServerConfig, ClientConfig
 @dataclass
 class GeneralConfig:
     exp_name: str = "exp1"
-    log_path: str = "logs"
-    data_path: str = "data"
+    log_path: str = "static/logs"
+    data_path: str = "static/data"
+    config_path: str = "static/config"
     seed: int = 42
     zmq_ip_address: str = "localhost"
     zmq_port: int = 5555
@@ -24,34 +24,24 @@ class GeneralConfig:
 
 # --- Global config cache ---
 _configs: Dict[str, Any] = {}
-_namespace_map = {
-    "general": ("general", GeneralConfig),
-    "server": ("fl_server", ServerConfig),
-    "client": ("fl_client", ClientConfig),
-    "dataset": ("dataset", ClientConfig),
-}
+cs = ConfigStore.instance()
 
 
-def setup_config(namespace: str):
+def setup_config(config_path, config_name, dataclass_type, overrides=None):
     """Initialize and cache a config namespace (Hydra + dataclass)."""
-    if namespace in _configs:
-        return _configs[namespace]
+    if config_name in _configs:
+        return _configs[config_name]
 
-    if namespace not in _namespace_map:
-        raise ValueError(f"Unknown config namespace: {namespace}")
+    cs.store(name=config_name, node=dataclass_type)
+    overrides = overrides or []
+    with initialize_config_dir(config_dir=config_path, version_base="1.3"):
+        yaml_cfg = compose(config_name=config_name, overrides=overrides)
 
-    config_name, dataclass_type = _namespace_map[namespace]
-    cs = ConfigStore.instance()
-    cs.store(name=namespace, node=dataclass_type)
-
-    config_dir = os.path.join(os.getcwd(), "static", "config")
-    with initialize_config_dir(config_dir=config_dir):
-        overrides = []#sys.argv[1:]
-        cfg = compose(config_name=config_name, overrides=overrides)
-
-    config_dict = OmegaConf.to_container(cfg, resolve=True)
-    _configs[namespace] = dataclass_type(**config_dict)
-    return _configs[namespace]
+    defaults = OmegaConf.structured(dataclass_type())
+    merged = OmegaConf.merge(defaults, yaml_cfg)
+    obj = OmegaConf.to_object(merged)
+    _configs[config_name] = obj
+    return obj
 
 
 def get_config(namespace: str):
