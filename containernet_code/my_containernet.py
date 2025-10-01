9a5bea701264f9d7f1753a6497f38a6e9a9cbccc
oabuhamdan <oabuhamdan@pm.me>
2025-09-30 23:05:11 -0500

    Improving start and stop experiment

--- containernet_code/my_containernet.py
+++ containernet_code/my_containernet.py
@@ -1,9 +1,11 @@
 from mininet.net import Containernet
 from mininet.node import Host
 
+from common.loggers import error
 from containernet_code.background_traffic.background_gen import BGTrafficRunner
 from containernet_code.experiment_runner import ExperimentRunner
 from containernet_code.my_topology import TopologyHandler
+import traceback
 
 
 class MyContainernet(Containernet):
@@ -17,27 +19,30 @@ class MyContainernet(Containernet):
         self.experiment_runner = experiment_runner
         self.fl_server_node = self.get(topo_handler.fl_server)
         self.fl_client_nodes = [self.get(client) for client in topo_handler.fl_clients]
+        self.experiment_runner.setup_nodes(self.fl_server_node, self.fl_client_nodes)
         if bg_runner:
             self.bg_client_nodes = {bg: self.get(bg) for bg in topo_handler.bg_clients}
             self.bg_runner.setup_nodes(self.bg_client_nodes)
-        self.experiment_runner.setup_nodes(self.fl_server_node, self.fl_client_nodes)
 
-    def start_experiment(self, logs=False, ping=True, aut_bg=True):
-        """Start the experiment with background traffic and monitoring."""
-        if ping:
-            print("Pinging FL hosts to ensure connectivity...")
-            self.ping_fl_hosts()
+    def start_experiment(self, logs=False, ping=True, auto_bg=True):
+        try:
+            """Start the experiment with background traffic and monitoring."""
+            if ping:
+                print("Pinging FL hosts to ensure connectivity...")
+                self.ping_fl_hosts()
 
-        if self.bg_runner and aut_bg:
-            print("Starting background traffic generation...")
-            self.bg_runner.start()
+            if self.bg_runner and auto_bg:
+                print("Starting background traffic generation...")
+                self.bg_runner.start()
 
-        print("Starting Experiment")
-        self.experiment_runner.start_experiment()
+            print("Starting Experiment")
+            self.experiment_runner.start_experiment()
 
-        if logs:
-            print("Following logs...")
-            self.follow_logs(self.fl_server_node)
+            if logs:
+                print("Following logs...")
+                self.follow_logs(self.fl_server_node)
+        except Exception as e:
+            error("Error during experiment start: {}\n{}".format(e, traceback.format_exc()))
 
     def follow_logs(self, client: Host, service: str = None):
         self.experiment_runner.follow_logs(client, service)
@@ -48,6 +53,10 @@ class MyContainernet(Containernet):
         self.experiment_runner.stop_experiment()
         self.bg_runner.stop()
         print("Experiment stopped successfully.")
+
+    def stop(self):
+        self.stop_experiment()
+        super().stop()
 
     def ping_fl_hosts(self):
         for client in self.fl_client_nodes:
